916ab216d9024ea8cdf4c19a64ac34a7e0af81c9
John Doe <johndoe@example.com>
2023-10-25 13:15:43 +0200

    last

--- app/Http/Controllers/UserFormController.php
+++ app/Http/Controllers/UserFormController.php
@@ -6,7 +6,19 @@ use Illuminate\Http\Request;
 
 class UserFormController extends Controller
 {
-    public function ShowUser(){
-        return 'form';
+    public function ShowUser(Request $req){
+        //return $req->input('firstName');
+        //return $req->all();
+        //return $req->method();
+        //return $req->url();
+        //return $req->path();
+
+        $data = [
+            'firstName' => $req->input('firstName'),
+            'lastName' => $req->input('lastName')
+        ];
+
+        return view('user', $data);
+
     }
 }
